47633b443317892c6b3612643db7bf8de94c1ff2
Jason Miesionczek <atmospherian@gmail.com>
2023-08-25 13:36:40 -0400

    Controller

---
+++ WatchStore/Controllers/CheckoutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WatchStore.Models;
+using WatchStore.Services;
+
+namespace WatchStore.Controllers
+{
+    [ApiController]
+    public class CheckoutController : Controller
+    {
+        private readonly CheckOutService checkOutService;
+
+        public CheckoutController(CheckOutService checkOutService)
+        {
+            this.checkOutService = checkOutService;
+        }
+        
+        [HttpPost("checkout")]
+        public CartResult Checkout([FromBody] string[] productIds)
+        {
+            var cart = this.checkOutService.CheckOutCart(productIds);
+
+            var totalPrice = this.checkOutService.GetCartResult(cart);
+
+            return totalPrice;
+        }
+    }
+}
